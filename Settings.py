ec0e9193d7f3430c2ab5b6052ffc42124fa5ce74
Hamza Ahmed <45544623+Hamza-101@users.noreply.github.com>
2024-12-27 07:55:45 +0500

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,24 +1,23 @@
 SimulationVariables = {
     "SimAgents" : 3,
     "AgentData" : [],
-    "SafetyRadius" : 2, 
+    "SafetyRadius" : 0.25, 
     "AccelerationInit" : 0.0,
-    "NeighborhoodRadius" : 6, 
+    "NeighborhoodRadius" : 2, 
     "VelocityUpperLimit" : 2.5,
     "AccelerationUpperLimit" : 5.0,
     "X" : 2,
     "Y" : 2,
     "dt" : 0.1,
-    "EvalTimeSteps" : 3000,
-    "LearningTimeSteps" :  200000,
-    "Episodes" : 20,
+    "EvalTimeSteps" : 600,
+    "LearningTimeSteps" : 1500000,
+    "Episodes" : 3,
     "LearningRate": 0.003,
     "NumEnvs": 6,
     "Counter" : 0,
     "Seed" : 23,
     "ModelSeed" : 19,
 }
-
 
 Files = {
     "Flocking" : 'Results\Flocking',
@@ -42,7 +41,6 @@ Results = {
     "EpisodalRewards" : "EpisodeRewards"
 }
 
-
 Animation = {
-    "TimeSteps" : 3000
+    "TimeSteps" : 600
 }
